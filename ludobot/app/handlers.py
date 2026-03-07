981d74161c1712b5e6708f1bceba9ef5220e43c4
Alexandr <sashhom@gmail.com>
2026-03-07 14:06:13 +0400

    Небольной дебаг

--- ludobot/app/handlers.py
+++ ludobot/app/handlers.py
@@ -13,14 +13,12 @@ ADMIN = os.getenv('ADMIN')
 logger = logging.getLogger(__name__)
 router = Router()
 
-@router.message(F.text)
-async def all_messages(message: Message):
-    await message.answer('Я не умею обшаться в личных чатах')
-
 @router.message(Command('send_scores'))
 async def send_scores(message: Message):
-    if message.from_user.id == ADMIN:
+    if message.from_user.id == int(ADMIN):
         await daily_send()
         await message.answer('Успешно')
+    else:
+        await message.answer('С новым годом, пошёл нафиг')
 
 
