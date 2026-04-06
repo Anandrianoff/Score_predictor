22310a0c421847d95d92941e906d141f5bcaa0dc
anandrianoff <andrey.andrianov2011@gmail.com>
2026-04-06 14:04:05 +0300

    fix

--- Utils/bacground_worker.py
+++ Utils/bacground_worker.py
@@ -31,7 +31,7 @@ Session = sessionmaker(engine)
 
 def update_games_info(date_from=None, date_to=None):
     date_from = date_from or (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")
-    date_to = date_to or (datetime.strptime(date_from, "%Y-%m-%d") + timedelta(days=1)).strftime("%Y-%m-%d")
+    date_to = date_to or (datetime.now() + timedelta(days=1)).strftime("%Y-%m-%d")
     request_url = Base_url + f"/games/list?leagueid=235&from={date_from}&to={date_to}"
     response = requests.get(request_url)
     data = response.json()
